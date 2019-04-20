7a31848d8fcf4f7a62b19d161bce4f8191316b33
Streaker <871486236@qq.com>
2019-04-21 00:15:27 +0800

    添加网站访问次数功能

--- src/test/java/com/streaker/springboot/FtpUtilTest.java
+++ src/test/java/com/streaker/springboot/FtpUtilTest.java
@@ -24,13 +24,13 @@ public class FtpUtilTest {
     public void testFtpClient() throws IOException {
         FTPClient ftpClient = new FTPClient();
         //System.out.println("#########");
-        ftpClient.connect("101.132.64.128",21);//服务器地址和端口
-        ftpClient.login("admin","admin");//登录的用户名和密码
+        ftpClient.connect("106.15.204.11",21);//服务器地址和端口
+        ftpClient.login("root1234","root1234");//登录的用户名和密码
         //读取本地文件，给出的是本地文件地址
         FileInputStream inputStream = new FileInputStream(new File("G:\\slide_3.jpg"));
         ftpClient.enterLocalActiveMode();
         //设置上传路径
-        ftpClient.changeWorkingDirectory("/home/admin");
+        ftpClient.changeWorkingDirectory("/home/images");
         //设置文件类型
         ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
         //1.服务器端保存的文件名，2.上传文件的inputstream
